f91375163f3cb2596627c89ad53efcce4a95c48e
Chris Williams <cwilliams@miamiherald.com>
2016-11-10 13:17:22 -0500

    Created slides.js

---
+++ src/javascripts/modules/utilities/slides.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import TweenLite from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+const loadSlides = () => {
+
+}
+
+export { loadSlides };
