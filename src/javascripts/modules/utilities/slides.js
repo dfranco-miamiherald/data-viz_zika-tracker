99399a4213e3c9210106253533cf52fefbd06413
Chris Williams <cwilliams@miamiherald.com>
2016-11-11 13:09:46 -0500

    Working prototype based off of ScrollMagic demo

--- src/javascripts/modules/utilities/slides.js
+++ src/javascripts/modules/utilities/slides.js
@@ -1,50 +1,39 @@
 import $ from 'jquery';
-import TimelineMax from 'gsap';
+import TweenMax from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 
-class Slide {
-  constructor() {
+const loadSlides = () => {
+  $(function () { // wait for document ready
     // init
-    this.controller = new ScrollMagic.Controller();
-
-    this.slideContainerEl = '#slideContainer';
-    this.pinContainerEl = '#pinContainer';
+    var controller = new ScrollMagic.Controller();
 
     // define movement of panels
-    this.wipeAnimation = new TimelineMax()
-			// animate to second panel
-			.to(this.slideContainerEl, 0.5, {z: -150})		// move back in 3D space
-			.to(this.slideContainerEl, 1,   {x: '-25%'})	// move in to first panel
-			.to(this.slideContainerEl, 0.5, {z: 0})				// move back to origin in 3D space
-			// animate to third panel
-			.to(this.slideContainerEl, 0.5, {z: -150, delay: 1})
-			.to(this.slideContainerEl, 1,   {x: '-50%'})
-			.to(this.slideContainerEl, 0.5, {z: 0})
-			// animate to forth panel
-			.to(this.slideContainerEl, 0.5, {z: -150, delay: 1})
-			.to(this.slideContainerEl, 1,   {x: '-75%'})
-			.to(this.slideContainerEl, 0.5, {z: 0});
-  }
+    var wipeAnimation = new TimelineMax()
+      // animate to second panel
+      .to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
+      .to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
+      .to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
+      // animate to third panel
+      .to("#slideContainer", 0.5, {z: -150, delay: 1})
+      .to("#slideContainer", 1,   {x: "-50%"})
+      .to("#slideContainer", 0.5, {z: 0})
+      // animate to forth panel
+      .to("#slideContainer", 0.5, {z: -150, delay: 1})
+      .to("#slideContainer", 1,   {x: "-75%"})
+      .to("#slideContainer", 0.5, {z: 0});
 
-  render() {
     // create scene to pin and link animation
-		new ScrollMagic.Scene({
-				triggerElement: this.pinContainerEl,
-				triggerHook: 'onLeave',
-				duration: '500%'
-			})
-			.setPin(this.pinContainerEl)
-			.setTween(wipeAnimation)
-			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
-  }
-}
-
-const loadSlides = () => {
-  $(document).ready(() => {
-    new Slide.render();
+    new ScrollMagic.Scene({
+        triggerElement: "#pinContainer",
+        triggerHook: "onLeave",
+        duration: "500%"
+      })
+      .setPin("#pinContainer")
+      .setTween(wipeAnimation)
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
   });
 }
 
