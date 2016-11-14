22f18e3e4905403a925657ed9ed9c34c54d8a725
Chris Williams <cwilliams@miamiherald.com>
2016-11-14 18:34:49 -0500

    use production url

--- src/javascripts/modules/visualizations/bubble-map.js
+++ src/javascripts/modules/visualizations/bubble-map.js
@@ -15,43 +15,44 @@ class BubbleMap {
     this.width = $(this.el).width() - this.margin.left - this.margin.right;
     this.height = Math.ceil(this.aspectRatio * (this.width - this.margin.top - this.margin.bottom));
     this.mapWidth = this.width;
-    this.shapeUrl = `data/florida-counties.json`;
-    this.dataColumn = `total`
-    this.totals = [`.bubble-map__stat--local`, `.bubble-map__stat--travel`, `.bubble-map__stat--total`]
+    // this.shapeUrl = 'data/florida-counties.json';
+    this.shapeUrl = 'http://pubsys.miamiherald.com/static/media/projects/2016/zika-interactive-v2/site/data/florida-counties.json';
+    this.dataColumn = 'total'
+    this.totals = ['.bubble-map__stat--local', '.bubble-map__stat--travel', '.bubble-map__stat--total']
   }
 
   render() {
-    this.svg = d3.select(this.el).append(`svg`)
-        .attr(`width`, `100%`)
-        .attr(`class`, `bubble-map__svg`)
-        .append(`g`);
+    this.svg = d3.select(this.el).append('svg')
+        .attr('width', '100%')
+        .attr('class', 'bubble-map__svg')
+        .append('g');
 
-    this.filter = this.svg.append("defs")
-        .append("filter")
-        .attr("id", "drop-shadow")
-        .attr("height", "130%");
+    this.filter = this.svg.append('defs')
+        .append('filter')
+        .attr('id', 'drop-shadow')
+        .attr('height', '130%');
 
-    this.filter.append("feGaussianBlur")
-        .attr("in", "SourceAlpha")
-        .attr("stdDeviation", 5)
-        .attr("result", "blur");
+    this.filter.append('feGaussianBlur')
+        .attr('in', 'SourceAlpha')
+        .attr('stdDeviation', 5)
+        .attr('result', 'blur');
 
-    this.filter.append("feOffset")
-        .attr("in", "blur")
-        .attr("dx", 5)
-        .attr("dy", 5)
-        .attr("result", "offsetBlur");
+    this.filter.append('feOffset')
+        .attr('in', 'blur')
+        .attr('dx', 5)
+        .attr('dy', 5)
+        .attr('result', 'offsetBlur');
 
-    this.feMerge = this.filter.append("feMerge");
+    this.feMerge = this.filter.append('feMerge');
 
-    this.feMerge.append("feMergeNode")
-        .attr("in", "offsetBlur")
-    this.feMerge.append("feMergeNode")
-        .attr("in", "SourceGraphic");
+    this.feMerge.append('feMergeNode')
+        .attr('in', 'offsetBlur')
+    this.feMerge.append('feMergeNode')
+        .attr('in', 'SourceGraphic');
 
     this.loadData();
     this.resizeBubbleMap();
-    $(window).on(`resize`, this.resizeBubbleMap.bind(this));
+    $(window).on('resize', this.resizeBubbleMap.bind(this));
   }
 
 
@@ -63,7 +64,7 @@ class BubbleMap {
       this.height = Math.ceil(this.aspectRatio * (this.width - this.margin.top - this.margin.bottom));
 
       TweenLite.set(chart, { scale: this.width / this.mapWidth });
-      d3.select(`.bubble-map__svg`).attr(`height`, this.height);
+      d3.select('.bubble-map__svg').attr('height', this.height);
     });
   }
 
@@ -79,9 +80,9 @@ class BubbleMap {
 
     this.shapeData = shapeData;
     this.caseData = caseData;
-    const counties = topojson.feature(this.shapeData, this.shapeData.objects[`florida-counties`]).features
+    const counties = topojson.feature(this.shapeData, this.shapeData.objects['florida-counties']).features
 
-    $(`.bubble-map__stat`).addClass(`is-animating`);
+    $('.bubble-map__stat').addClass('is-animating');
     this.drawSlider();
     this.drawTooltip();
     this.totals.forEach(i => {
@@ -89,47 +90,47 @@ class BubbleMap {
     });
 
     this.projection = d3.geoEquirectangular()
-      .fitSize([this.width, this.height], topojson.feature(this.shapeData, this.shapeData.objects[`florida-counties`]));
+      .fitSize([this.width, this.height], topojson.feature(this.shapeData, this.shapeData.objects['florida-counties']));
 
     this.path = d3.geoPath()
       .projection(this.projection);
 
-    this.svg.append(`g`)
-        .attr(`class`, `bubble-map__counties`)
-        .style("filter", "url(#drop-shadow)")
-      .selectAll(`path`)
+    this.svg.append('g')
+        .attr('class', 'bubble-map__counties')
+        .style('filter', 'url(#drop-shadow)')
+      .selectAll('path')
         .data(counties)
-      .enter().append(`path`)
-        .attr(`class`, `bubble-map__county`)
-        .attr(`d`, this.path);
+      .enter().append('path')
+        .attr('class', 'bubble-map__county')
+        .attr('d', this.path);
 
     this.max = this.caseData[0].totalTravel;
     this.radius = d3.scaleSqrt()
         .domain([0, this.max])
         .range([0, 5]);
 
-    this.svg.append(`g`)
-        .attr(`class`, `bubble-map__bubble`)
-      .selectAll(`circle`)
-        .data(topojson.feature(this.shapeData, this.shapeData.objects[`florida-counties`]).features
+    this.svg.append('g')
+        .attr('class', 'bubble-map__bubble')
+      .selectAll('circle')
+        .data(topojson.feature(this.shapeData, this.shapeData.objects['florida-counties']).features
           .sort((a, b) => {
             if (this.caseData[this.caseData.length - 1].counties[b.id] && this.caseData[this.caseData.length - 1].counties[a.id]) {
               return this.caseData[this.caseData.length - 1].counties[b.id][this.dataColumn] - this.caseData[this.caseData.length - 1].counties[a.id][this.dataColumn]
             }
           }))
-      .enter().append(`circle`)
-        .attr(`transform`, (d) => `translate(${this.path.centroid(d)})`)
-        .attr(`r`, (d) => {
+      .enter().append('circle')
+        .attr('transform', (d) => `translate(${this.path.centroid(d)})`)
+        .attr('r', (d) => {
           if (this.caseData[this.unformatSlider()].counties[d.id]) {
             return this.radius(this.caseData[this.unformatSlider()].counties[d.id][this.dataColumn]);
           }
         })
-        .on(`mouseover`, (d) => {
+        .on('mouseover', (d) => {
           this.mouse = d3.mouse(this.svg.node()).map((d) => parseInt(d))
           this.tooltip
-            .classed(`is-active`, true)
-            .style(`left`, `${this.mouse[0]}px`)
-            .style(`top`, `${this.mouse[1]}px`)
+            .classed('is-active', true)
+            .style('left', `${this.mouse[0]}px`)
+            .style('top', `${this.mouse[1]}px`)
             .html(() => {
               if (this.caseData[this.unformatSlider()].counties[d.id][this.dataColumn] > 1) {
                 return `${d.properties.county}: ${this.caseData[this.unformatSlider()].counties[d.id][this.dataColumn]} cases`
@@ -138,14 +139,14 @@ class BubbleMap {
               }
             });
         })
-        .on(`mouseout`, (d) => {
+        .on('mouseout', (d) => {
           this.tooltip
-            .classed(`is-active`, false);
+            .classed('is-active', false);
         });
 
 
-    this.stepSlider.noUiSlider.on(`update`, this.resizeBubbles.bind(this))
-    this.stepSlider.noUiSlider.on(`update`, () => {
+    this.stepSlider.noUiSlider.on('update', this.resizeBubbles.bind(this))
+    this.stepSlider.noUiSlider.on('update', () => {
       this.totals.forEach(i => {
         this.setTotals(i);
         this.setDate();
@@ -156,8 +157,8 @@ class BubbleMap {
 
   drawTooltip() {
     this.tooltip = d3.select(this.el)
-      .append(`div`)
-      .attr(`class`, `bubble-map__tooltip`);
+      .append('div')
+      .attr('class', 'bubble-map__tooltip');
   }
 
   drawSlider () {
@@ -177,13 +178,13 @@ class BubbleMap {
   }
 
   resizeBubbles() {
-    this.dataColumn = $(`.tabs__link.is-active`).data(`case`);
+    this.dataColumn = $('.tabs__link.is-active').data('case');
 
     this.svg
-      .selectAll(`circle`)
+      .selectAll('circle')
         .transition()
         .duration(750)
-        .attr(`r`, (d) => {
+        .attr('r', (d) => {
           if (this.caseData[this.unformatSlider()].counties[d.id]) {
             return this.radius(this.caseData[this.unformatSlider()].counties[d.id][this.dataColumn]);
           }
@@ -192,9 +193,9 @@ class BubbleMap {
   }
 
   switchTabs() {
-    $(`.tabs__link`).click(() => {
-      $(`.tabs__link`).removeClass(`is-active`);
-      $(event.currentTarget).addClass(`is-active`);
+    $('.tabs__link').click(() => {
+      $('.tabs__link').removeClass('is-active');
+      $(event.currentTarget).addClass('is-active');
 
       this.resizeBubbles();
     })
@@ -202,15 +203,15 @@ class BubbleMap {
 
   setTotals(el) {
     var counterStart = {var:$(el).text().replace(/(?:\D)+/, +'')};
-    if (el === `.bubble-map__stat--local`) {
+    if (el === '.bubble-map__stat--local') {
       var counterEnd = {var: this.caseData[this.unformatSlider()].totalLocal};
-      var text = `Local`;
-    } else if (el === `.bubble-map__stat--travel`) {
+      var text = 'Local';
+    } else if (el === '.bubble-map__stat--travel') {
       var counterEnd = {var: this.caseData[this.unformatSlider()].totalTravel};
-      var text = `Travel`;
-    } else if (el === `.bubble-map__stat--total`) {
+      var text = 'Travel';
+    } else if (el === '.bubble-map__stat--total') {
       var counterEnd = {var: +this.caseData[this.unformatSlider()].totalLocal + +this.caseData[this.unformatSlider()].totalTravel};
-      var text = `Total`;
+      var text = 'Total';
     }
 
     TweenMax.to(counterStart, 1.5, {var: counterEnd.var, onUpdate: () => {
@@ -225,7 +226,7 @@ class BubbleMap {
   }
 
   setDate() {
-    $(`#js-date`).html(moment(this.caseData[this.unformatSlider()].date).format(`MMM Do YYYY`));
+    $('#js-date').html(moment(this.caseData[this.unformatSlider()].date).format('MMM Do YYYY'));
   }
 
 }
