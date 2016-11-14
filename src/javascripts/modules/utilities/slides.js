ef1d3ab7361c9bbf7ae295ae763adf84965a5a7e
Chris Williams <cwilliams@miamiherald.com>
2016-11-14 16:30:27 -0500

    Reverted to sliding panels

--- src/javascripts/modules/utilities/slides.js
+++ src/javascripts/modules/utilities/slides.js
@@ -3,41 +3,52 @@ import TweenMax from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+window.$ = $;
 
-class SlideAnimation {
-  constructor() {
-    this.slideContainerEl = '#slideContainer';
-    this.pinContainerEl = '#pinContainer';
-    this.duration = '1000%';
-
+class Slides {
+  constructor(el) {
+    this.el = el;
+    this.slides = ['#panel-1', '#panel-2', '#panel-3', '#panel-4'];
     this.controller = new ScrollMagic.Controller();
     this.wipeAnimation = new TimelineMax()
-        .to(this.slideContainerEl, 0.5, {z: -150})
-        .to(this.slideContainerEl, 1,   {x: "-25%"})
-        .to(this.slideContainerEl, 0.5, {z: 0})
-        .to(this.slideContainerEl, 0.5, {z: -150, delay: 1})
-        .to(this.slideContainerEl, 1,   {x: "-50%"})
-        .to(this.slideContainerEl, 0.5, {z: 0})
-        .to(this.slideContainerEl, 0.5, {z: -150, delay: 1})
-        .to(this.slideContainerEl, 1,   {x: "-75%"})
-        .to(this.slideContainerEl, 0.5, {z: 0});
+        .fromTo(this.slides[1], 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone })
+        .fromTo(this.slides[2], 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone })
+        .fromTo(this.slides[3], 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone })
+
+    $(this.el).height($(window).height());
+    this.setContainerHeight();
+    $(window).resize(this.setContainerHeight.bind(this));
+  }
+
+  setContainerHeight() {
+    window.requestAnimationFrame(() => {
+      $(this.el).height($(window).height());
+      $(this.el).width($(window).width());
+      this.slides.forEach((i) => {
+        $(i).height($(this.el).height());
+        $(i).width($(this.el).width());
+      });
+    });
   }
 
   render() {
     new ScrollMagic.Scene({
-        triggerElement: this.pinContainerEl,
+        triggerElement: this.el,
         triggerHook: 'onLeave',
-        duration: this.duration
-      })
-      .setPin(this.pinContainerEl)
-      .setTween(this.wipeAnimation)
-      .addIndicators() // remove for production
-      .addTo(this.controller);
+        duration: '300%'
+    })
+    .setPin(this.el)
+    .setTween(this.wipeAnimation)
+    .addIndicators()
+    .addTo(this.controller);
   }
 }
 
 const loadSlides = () => {
-  new SlideAnimation().render();
+  const $pinContainer = $('#pinContainer');
+  const id = $pinContainer.attr('id');
+
+  new Slides(`#${id}`).render();
 }
 
-export { loadSlides };
+export { loadSlides }
