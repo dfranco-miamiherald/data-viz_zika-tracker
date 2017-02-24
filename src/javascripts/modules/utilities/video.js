ea7ae2adb98930f0a708d21ddced390bccd607a2
Chris Williams <cwilliams@miamiherald.com>
2017-02-24 11:35:11 -0500

    remove container main padding

--- src/javascripts/modules/utilities/video.js
+++ src/javascripts/modules/utilities/video.js
@@ -5,6 +5,10 @@ const loadVideos = () => {
   $video.on('canplaythrough', function() {
      this.play();
   });
+
+  // Override the main container padding
+  $('.container[role="main"]').css({ 'padding': '0px' });
+  $('.container[role="main"] .col-sm-12').css({ 'padding': '0px' });
 };
 
 export { loadVideos };
