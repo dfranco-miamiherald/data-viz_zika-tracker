8033bb7ae0387603a0b11562b7713dc7f58bb369
Chris Williams <cwilliams@miamiherald.com>
2017-02-24 11:55:37 -0500

    fixed js syntax err

--- src/javascripts/modules/utilities/video.js
+++ src/javascripts/modules/utilities/video.js
@@ -8,7 +8,7 @@ const loadVideos = () => {
 
   // Override the main container padding
   $('.container[role="main"], .container[role="main"] .col-sm-12').css({
-    'padding-left': '0px'
+    'padding-left': '0px',
     'padding-right': '0px'
   });
 };
