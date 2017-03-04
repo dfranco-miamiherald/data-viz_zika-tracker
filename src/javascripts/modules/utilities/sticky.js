9b9da2eb5ec00342a4eacc0a8cdba30cf4d53188
Chris Williams <cwilliams@miamiherald.com>
2017-03-03 20:05:15 -0500

    social updates

--- src/javascripts/modules/utilities/sticky.js
+++ src/javascripts/modules/utilities/sticky.js
@@ -1,61 +1,69 @@
 import $ from 'jquery';
+import { TweenLite, TweenMax } from 'gsap';
+import ScrollMagic from 'scrollmagic';
 
 const loadSticky = () => {
-  var stickyHeaders = (function() {
+  // var stickyHeaders = (function() {
+  //
+  //   var $window = $(window),
+  //       $stickies;
+  //
+  //   var load = function(stickies) {
+  //
+  //     if (typeof stickies === "object" && stickies.length > 0) {
+  //       $stickies = stickies.each(function() {
+  //         var $thisSticky = $(this).wrap('<div class="followWrap" />');
+  //         $thisSticky
+  //             .data('originalPosition', $thisSticky.offset().top)
+  //             .data('originalHeight', $thisSticky.outerHeight())
+  //               .parent()
+  //               .height($thisSticky.outerHeight());
+  //       });
+  //
+  //       $window.off("scroll.stickies").on("scroll.stickies", function() {
+  //   		  _whenScrolling();
+  //       });
+  //     }
+  //   };
+  //
+  //   var _whenScrolling = function() {
+  //
+  //     $stickies.each(function(i) {
+  //
+  //       var $thisSticky = $(this),
+  //           $stickyPosition = $thisSticky.data('originalPosition');
+  //
+  //       if ($stickyPosition <= $window.scrollTop()) {
+  //         var $nextSticky = $stickies.eq(i + 1),
+  //             $nextStickyPosition = $nextSticky.data('originalPosition') - $thisSticky.data('originalHeight');
+  //
+  //         $thisSticky.addClass("fixed");
+  //       } else {
+  //         var $prevSticky = $stickies.eq(i - 1);
+  //
+  //         $thisSticky.removeClass("fixed");
+  //
+  //         if ($prevSticky.length > 0 && $window.scrollTop() <= $thisSticky.data('originalPosition') - $thisSticky.data('originalHeight')) {
+  //           $prevSticky.removeClass("absolute").removeAttr("style");
+  //         }
+  //       }
+  //     });
+  //   };
+  //
+  //   return {
+  //     load: load
+  //   };
+  // })();
+  //
+  // $(function() {
+  //   stickyHeaders.load($(".followMeBar"));
+  // });
+  var controller = new ScrollMagic.Controller();
 
-    var $window = $(window),
-        $stickies;
-
-    var load = function(stickies) {
-
-      if (typeof stickies === "object" && stickies.length > 0) {
-        $stickies = stickies.each(function() {
-          var $thisSticky = $(this).wrap('<div class="followWrap" />');
-          $thisSticky
-              .data('originalPosition', $thisSticky.offset().top)
-              .data('originalHeight', $thisSticky.outerHeight())
-                .parent()
-                .height($thisSticky.outerHeight());
-        });
-
-        $window.off("scroll.stickies").on("scroll.stickies", function() {
-    		  _whenScrolling();
-        });
-      }
-    };
-
-    var _whenScrolling = function() {
-
-      $stickies.each(function(i) {
-
-        var $thisSticky = $(this),
-            $stickyPosition = $thisSticky.data('originalPosition');
-
-        if ($stickyPosition <= $window.scrollTop()) {
-          var $nextSticky = $stickies.eq(i + 1),
-              $nextStickyPosition = $nextSticky.data('originalPosition') - $thisSticky.data('originalHeight');
-
-          $thisSticky.addClass("fixed");
-        } else {
-          var $prevSticky = $stickies.eq(i - 1);
-
-          $thisSticky.removeClass("fixed");
-
-          if ($prevSticky.length > 0 && $window.scrollTop() <= $thisSticky.data('originalPosition') - $thisSticky.data('originalHeight')) {
-            $prevSticky.removeClass("absolute").removeAttr("style");
-          }
-        }
-      });
-    };
-
-    return {
-      load: load
-    };
-  })();
-
-  $(function() {
-    stickyHeaders.load($(".followMeBar"));
-  });
+  var scene = new ScrollMagic.Scene({triggerElement: "#pin2"})
+  	.setPin("#pin2")
+  	.addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
+  	.addTo(controller);
 };
 
 export { loadSticky };
