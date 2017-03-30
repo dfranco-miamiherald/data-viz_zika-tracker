d9e75157d010e9832662d0a5e7a9f6081ddf01ec
Chris Williams <cwilliams@miamiherald.com>
2017-03-30 16:33:09 -0400

    added viewport logic

--- src/javascripts/modules/utilities/scroll.js
+++ src/javascripts/modules/utilities/scroll.js
@@ -21,7 +21,9 @@ const loadScroll = () => {
     );
   });
 
-  $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />');
+  document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+  });
 };
 
 export { loadScroll };
