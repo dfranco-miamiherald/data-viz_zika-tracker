712f9271a8f9983a9d206f0778e258e8ea20d1ad
Chris Williams <cwilliams@miamiherald.com>
2017-03-30 16:16:59 -0400

    added head viewport script

--- src/javascripts/modules/utilities/scroll.js
+++ src/javascripts/modules/utilities/scroll.js
@@ -20,6 +20,8 @@ const loadScroll = () => {
       }
     );
   });
+
+  $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />');
 };
 
 export { loadScroll };
